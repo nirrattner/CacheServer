c8ed11662281bed24a0ada44842ab13dbcea0176
Nir Rattner <nirrattner@gmail.com>
2023-03-15 20:57:35 -0400

    Fix enums

--- scripts/cache_client.py
+++ scripts/cache_client.py
@@ -13,63 +13,24 @@ INPUT_HEADER_FILE = CURRENT_DIRECTORY + '/../src/cache_communication_protocol.h'
 START_PATTERN = re.compile('(typedef enum|typedef struct) {')
 END_PATTERN = re.compile('} (.*)_t;')
 
-class CacheProtocol:
-  def __init__(self):
-    self.protocol = {}
-    with open(INPUT_HEADER_FILE, 'r') as input_file:
-      lines = []
-      for line in input_file.readlines():
-        result = START_PATTERN.search(line)
-        if result:
-          if result.group(1) == 'typedef enum':
-              kind = 'enum'
-          else:
-              kind = 'struct'
-          lines = []
-        lines.append(line)
+class CacheProtocolStructInstance:
+  def __init__(self, definition):
+    self.definition = definition
+    buffer = b'\00' * definition['size']
+    self.fields = c2py.depack_bytearray_to_dict(buffer, self.definition['text'], '>')
 
-        result = END_PATTERN.search(line)
-        if result:
-          name = result.group(1)
-          text = '\n'.join(lines)
-          if kind == 'struct':
-            size = c2py.structSize(text, '>')
-            vlist, pformat = c2py.structInfo(text, '>')
-          elif kind == 'enum':
-            size = 0
-            lines = lines[1:-1]
-            lines = [re.sub('[ ,\n]', '', value) for value in lines]
-            lines = [value.split('=')[0] for value in lines]
-            vlist = lines
-            pformat = {value: index for index, value in enumerate(lines)}
-
-          self.protocol[name] = dict(
-              format=pformat,
-              vlist=vlist,
-              size=size,
-              text=text,
-              kind=kind)
-
-  def define(self, type):
-    buffer = b'\00' * self.protocol[type]['size']
-    value = c2py.depack_bytearray_to_dict(buffer, self.protocol[type]['text'], '>')
-    value['__TYPE'] = type
-    return value
-
-  def enum(self, type, value):
-    return self.protocol[type.lower()]['format'][type.upper() + '__' + value.upper()]
-
-  def pack_dict_to_bytearray(self, struct_instance, struct_definition, alignment="="):
-    variable_list, pack_format = c2py.structInfo(struct_definition, alignment)
+  def pack(self, alignment='>'):
     variables = []
+    pack_format = self.definition['pack_format']
+    variable_list = self.definition['variable_list']
 
     for variable_name, array_length in variable_list:
       if array_length > 1:
-        variables.append(struct_instance[variable_name])
-        if array_length > len(struct_instance[variable_name]):
-          variables += '\0' * (array_length - len(struct_instance[variable_name]))
+        variables.append(self.fields[variable_name])
+        if array_length > len(self.fields[variable_name]):
+          variables += '\0' * (array_length - len(self.fields[variable_name]))
       else:
-        variables.append(struct_instance[variable_name])
+        variables.append(self.fields[variable_name])
 
     for index in range(len(variables)):
       if pack_format[index + 1] == 'c' and isinstance(variables[index], int):
@@ -80,35 +41,74 @@ class CacheProtocol:
 
     return buffer
 
-  def pack(self, struct_instance):
-    type = struct_instance['__TYPE']
-    struct_instance_copy = dict(struct_instance)
-    del struct_instance_copy['__TYPE']
-    struct = self.pack_dict_to_bytearray(struct_instance_copy, self.protocol[type]['text'], '>')
-    return struct
+  def unpack(self, input_data):
+    self.fields = c2py.depack_bytearray_to_dict(input_data, self.definition['text'], '>')
 
-  def unpack(self, struct_instance, input_data):
-    type = struct_instance['__TYPE']
-    struct = c2py.depack_bytearray_to_dict(input_data, self.protocol[type]['text'], '>')
-    struct['__TYPE'] = type
-    return struct
+class CacheProtocol:
+  def __init__(self):
+    self.definitions = {}
+    with open(INPUT_HEADER_FILE, 'r') as input_file:
+      lines = []
+      for line in input_file.readlines():
+        result = START_PATTERN.search(line)
+        if result:
+          if result.group(1) == 'typedef enum':
+              kind = 'enum'
+          else:
+              kind = 'struct'
+          lines = []
+        line = line.split('//')[0].strip()
+        if line:
+          lines.append(line)
 
-protocol = CacheProtocol()
+        result = END_PATTERN.search(line)
+        if result:
+          name = result.group(1)
+          text = '\n'.join(lines)
+          if kind == 'struct':
+            size = c2py.structSize(text, '>')
+            variable_list, pack_format = c2py.structInfo(text, '>')
+          elif kind == 'enum':
+            size = 0
+            pack_format = None
+            entries = [re.sub('[ ,\n]', '', value) for value in lines[1:-1]]
 
-request_header = protocol.define('request_header')
-request_header['version'] = 0xff 
-request_header['flags'] = 123
-request_header['type'] = protocol.enum('REQUEST_TYPE','PUT')
+            next_value = 0
+            variable_list = {}
+            for entry in entries:
+              entry_splits = entry.split('=')
+              if len(entry_splits) > 1:
+                next_value = eval(entry_splits[1])
+              variable_list[entry_splits[0]] = next_value
+              next_value += 1
 
-packed_header = protocol.pack(request_header)
-print(f'packed_header: {packed_header}')
+          self.definitions[name] = dict(
+              pack_format=pack_format,
+              variable_list=variable_list,
+              size=size,
+              text=text)
 
+  def allocate(self, type):
+    return CacheProtocolStructInstance(self.definitions[type])
 
-new_request_header = protocol.define('request_header')
-new_request_header
+  def enum(self, type, value):
+    return self.definitions[type.lower()]['variable_list'][type.upper() + '__' + value.upper()]
 
-new_request_header = protocol.unpack(new_request_header, packed_header)
-print(f'new_request_header["version"] {new_request_header["version"]}')
-print(f'new_request_header["flags"] {new_request_header["flags"]}')
-print(f'new_request_header["type"] {new_request_header["type"]}')
+if __name__ == '__main__':
+  protocol = CacheProtocol()
+
+  request_header = protocol.allocate('request_header')
+  request_header.fields['version'] = 0xff
+  request_header.fields['flags'] = 123
+  request_header.fields['type'] = protocol.enum('REQUEST_TYPE','PUT')
+
+  packed_header_buffer = request_header.pack()
+  print(f'packed_header_buffer: {packed_header_buffer}')
+
+  new_request_header = protocol.allocate('request_header')
+
+  new_request_header.unpack(packed_header_buffer)
+  print(f'new_request_header.fields["version"] {new_request_header.fields["version"]}')
+  print(f'new_request_header.fields["flags"] {new_request_header.fields["flags"]}')
+  print(f'new_request_header.fields["type"] {new_request_header.fields["type"]}')
 
